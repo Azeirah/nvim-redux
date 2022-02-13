90ce5a5a64942d4c3db956310bb430a84aec9ca1
laura <mail@laura.fm>
2022-02-13 20:18:25 +0100

    Refactor: Move file reading utility to new utils file

---
+++ utils.lua
@@ -0,0 +1,10 @@
+local function read_file_contents(filename)
+    f = io.open(filename, 'r')
+    content = f:read('a')
+    f.close()
+    return content
+end
+
+return {
+    read_file_contents = read_file_contents
+}
