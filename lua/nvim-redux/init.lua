18af65973b0dee52e0880badb6f59a3063957001
laura <mail@laura.fm>
2022-02-14 22:00:04 +0100

    Fix: allow file reading to be relative to current source dir

--- lua/nvim-redux/init.lua
+++ lua/nvim-redux/init.lua
@@ -3,7 +3,7 @@ local utils = require('nvim-redux.utils')
 local rgts_query = require('nvim-redux.rgts_query')
 
 local function do_the_thing(cwd)
-    local switch_case_names_query = utils.read_file_contents('query_switch.tsq')
+    local switch_case_names_query = utils.read_file_contents('queries/query_switch.tsq', true)
     local captures = rgts_query.query_for_telescope("action\\.type", switch_case_names_query, cwd)
 
     redux_picker(captures)
--- lua/nvim-redux/utils.lua
+++ lua/nvim-redux/utils.lua
@@ -1,4 +1,19 @@
-local function read_file_contents(filename)
+debug_utils = require('plenary.debug_utils')
+
+local function get_path_relative_to_source_dir(filename)
+    local sourced_file = debug_utils.sourced_filepath()
+    local base_dir = vim.fn.fnamemodify(sourced_file, ":p:h")
+
+    return base_dir .. "/" .. filename
+end
+
+local function read_file_contents(filename, relative)
+    if relative then
+        filename = get_path_relative_to_source_dir(filename)
+    end
+
+    print('filename::' .. filename)
+
     local f = io.open(filename, 'r')
     content = f:read('a')
     f.close()
