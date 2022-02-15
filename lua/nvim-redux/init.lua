8ece52866ac9f05e39c37ca7b78aae1670005d1d
laura <mail@laura.fm>
2022-02-15 11:27:55 +0100

    Add: find dispatch using live_grep defaults

--- lua/nvim-redux/init.lua
+++ lua/nvim-redux/init.lua
@@ -1,12 +1,22 @@
 local redux_picker = require('nvim-redux.telescope-redux-picker')
 local utils = require('nvim-redux.utils')
 local rgts_query = require('nvim-redux.rgts_query')
+local telescope = require('telescope.builtin')
 
-local function do_the_thing(cwd)
+local function raw_redux_actions_in_reducers()
     local switch_case_names_query = utils.read_file_contents('queries/query_switch.tsq', true)
-    local captures = rgts_query.query_for_telescope("action\\.type", switch_case_names_query, cwd)
+    local captures = rgts_query.query_for_telescope("action\\.type", switch_case_names_query)
 
     redux_picker(captures)
+end
+
+local function find_dispatch_calls()
+    telescope.live_grep({
+        default_text="dispatch",
+        prompt_title="redux dispatch calls",
+        additional_args=function()
+            return {"-g*ts", "-g*js", "-g*tsx", "-g*jsx"}
+        end})
 end
 
 return {
