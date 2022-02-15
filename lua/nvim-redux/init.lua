c8fb274078b9f7a250f155fcf016af8f85a0a703
laura <mail@laura.fm>
2022-02-15 11:33:57 +0100

    Add: In dispatch only search for isolated word

--- lua/nvim-redux/init.lua
+++ lua/nvim-redux/init.lua
@@ -12,7 +12,7 @@ end
 
 local function find_dispatch_calls()
     telescope.live_grep({
-        default_text="dispatch\\s*\\(",
+        default_text="\\s*dispatch\\s*\\(",
         prompt_title="redux dispatch calls",
         additional_args=function()
             return {"-g*ts", "-g*js", "-g*tsx", "-g*jsx"}
