2b1d14c830b59047c8a094f83df2b9431cfd4595
laura <mail@laura.fm>
2022-02-23 20:46:14 +0100

    Add: Query and test file for reducers defined in slices

---
+++ lua/nvim-redux/queries/query_slice.scm
@@ -0,0 +1,11 @@
+(variable_declarator 
+    value: (call_expression 
+        function: 
+            (identifier) @fn_name
+            (#match? @fn_name "createSlice")
+        arguments: (arguments (object (pair
+                key: 
+                    (property_identifier) @key_name 
+                    (#match? @key_name "reducers")
+                value: (object (method_definition 
+                    name: (property_identifier) @reducer)))))))
---
+++ lua/tests/redux_slice.js
@@ -0,0 +1,23 @@
+// this file is based on the redux tutorial
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = { value: 0 }
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment(state) {
+      state.value++
+    },
+    decrement(state) {
+      state.value--
+    },
+    incrementByAmount(state, action) {
+      state.value += action.payload
+    },
+  },
+})
+
+export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export default counterSlice.reducer
